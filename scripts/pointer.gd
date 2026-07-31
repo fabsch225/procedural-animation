e3178bfb6364b0e26bb25b2b01a18b37208e525d
BaconEggsRL <apatton090@gmail.com>
2026-07-30 21:28:21 -0400

    yay

--- scripts/pointer.gd
+++ scripts/pointer.gd
@@ -2,15 +2,39 @@ class_name Pointer
 extends HotswapMouseCursor
 
 
+@export_group("Pointer Mode")
+@export var hotswap_enabled: bool = true
+@export var override_cursor: Texture2D
+@export var override_hotspot: Vector2 = Vector2.ZERO
+
 @onready var file_name_label: Label = $FileNameLabel
 
 
 func _ready() -> void:
-	super._ready()
-	cursor_changed.connect(_on_cursor_changed)
+	if hotswap_enabled:
+		file_name_label.visible = true
+		super._ready()
+		cursor_changed.connect(_on_cursor_changed)
 
-	if cursor_index >= 0:
-		_on_cursor_changed(cursor_paths[cursor_index], cursor_index)
+		if cursor_index >= 0:
+			_on_cursor_changed(cursor_paths[cursor_index], cursor_index)
+	else:
+		file_name_label.visible = false
+		_apply_override_cursor()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if hotswap_enabled:
+		super._unhandled_input(event)
+
+
+func _apply_override_cursor() -> void:
+	Input.set_custom_mouse_cursor(override_cursor, cursor_shape, override_hotspot)
+
+	if override_cursor != null:
+		file_name_label.text = override_cursor.resource_path.get_file()
+	else:
+		file_name_label.text = "System cursor"
 
 
 func _on_cursor_changed(cursor_path: String, _index: int) -> void:
