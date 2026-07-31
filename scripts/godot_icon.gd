99a0f8faffa7682717fd87dc412f2e42f7de998f
BaconEggsRL <apatton090@gmail.com>
2026-07-30 22:39:23 -0400

    asd

---
+++ scripts/godot_icon.gd
@@ -0,0 +1,10 @@
+extends Sprite2D
+
+
+func _ready() -> void:
+	get_viewport().size_changed.connect(_center_in_viewport)
+	_center_in_viewport()
+
+
+func _center_in_viewport() -> void:
+	position = get_viewport_rect().get_center()
